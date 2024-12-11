4a44197dedf09a5696fd49944673eded490ea0ed
layzeekid47 <layzeekid47@gmail.com>
2024-12-10 22:32:47 -0500

    Create Script: act2script1.sh

---
+++ Script: act2script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script: act2script1.sh
+
+echo "Enter your login details:" > login.txt
+echo "Username: your_username_here" >> login.txt
+echo "Password: your_password_here" >> login.txt
+echo "Login information saved in login.txt."
